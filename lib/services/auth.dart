672a41ea80df3f645e152fa948e21ed611fca246
A-Mulvihill <mulvihad@tcd.ie>
2021-03-15 16:25:05 +0000

    Added to the dependencies in the build.gradle file to allow use of firebase authentication.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,11 +1,10 @@
 //this file defines methods that will interact with firebase authentication
 
-//import firebase for authentication library here later
+//import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService{
 
   //this line to declare final private property type firebase object here FirebaseAuth
-
   
   //sign in anonymously(for now)
 
