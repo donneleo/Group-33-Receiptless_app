e5970913f714ca9b987ce15da85fddb1cf8f306f
RenaldasSapaitis <rsapaitis@gmail.com>
2021-04-21 09:56:19 +0100

    Worked on ReceiptRoute.dart further, now able to display all elements of our firestore collection in the app!

--- lib/receipts/ReceiptRoute.dart
+++ lib/receipts/ReceiptRoute.dart
@@ -36,10 +36,13 @@ class ReceiptRoute extends StatelessWidget {
           return ListView(
             children: snapshot.data.docs.map((document){
               return Center(
+                // hard code, would be a good idea to implement dynamic attribute allocation, i.e. be able to calculate Change by subtracting Price from Paid
+                // not the best
                 child: Container(
+                  padding: EdgeInsets.only(top:20.0),
                   width: MediaQuery.of(context).size.width / 1.2,
-                  height: MediaQuery.of(context).size.height / 4,
-                  child: Text("Product: " + document['Product']),
+                  height: MediaQuery.of(context).size.height / 6,
+                  child: Text("Product: " + document['Product']+ "\n" + "Price: " + document['Price'].toString() + "\n" +"Paid: " + document['Paid'].toString()+ "\n" + "Change: "  + document['Change'].toString() +"\n"+ "Date: " + document['Date']),
 
                 ),
               );
